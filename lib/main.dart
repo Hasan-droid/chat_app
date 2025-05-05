751e7f7f6f1344755c374083a2aa4dad7320dac0
Hasan Baydoun <hasan-net2010@hotmail.com>
2025-05-06 00:44:55 +0300

    281.Showing Different Screens Based On The Authentication State
    1- using streamBuilder widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,19 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:chat_app/screens/auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:chat_app/screens/chat.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(const App());
+  runApp(App());
 }
 
 class App extends StatelessWidget {
-  const App({super.key});
+  App({super.key});
+  final fireBaseAuthInstance = FirebaseAuth.instance;
 
   @override
   Widget build(BuildContext context) {
@@ -20,7 +23,15 @@ class App extends StatelessWidget {
         useMaterial3: true,
         colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 63, 17, 177)),
       ),
-      home: authScreen(),
+      home: StreamBuilder(
+        stream: fireBaseAuthInstance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return chatScreen();
+          }
+          return authScreen();
+        },
+      ),
     );
   }
 }
---
+++ lib/screens/chat.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class chatScreen extends StatelessWidget {
+  chatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(appBar: AppBar(title: Text("chat")), body: Center(child: Text("loggedIn!")));
+  }
+}
