8a9f639a673983c00c92e187178a33b95f147699
Hasan Baydoun <hasan-net2010@hotmail.com>
2025-05-09 20:22:50 +0300

    296. Sending & Reading Data To & From Firestore 1-295 was just a document to read

--- lib/widgets/new_message.dart
+++ lib/widgets/new_message.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class NewMessage extends StatefulWidget {
@@ -17,12 +19,29 @@ class _NewMessageState extends State<NewMessage> {
     super.dispose();
   }
 
-  void submit() {
+  void submit() async {
     var _enterdMessage = _inputController.text;
 
     if (_enterdMessage.trim().isEmpty) return;
 
     _inputController.clear();
+
+    //close anything on screen
+    FocusScope.of(context).unfocus();
+
+    final user = await FirebaseAuth.instance.currentUser!;
+    final userData =
+        await FirebaseFirestore.instance
+            .collection("users")
+            .doc(user.uid)
+            .get();
+
+    FirebaseFirestore.instance.collection("chat").add({
+      "createdAt": Timestamp.now(),
+      "message": _enterdMessage,
+      "userId": user.uid,
+      "image": userData.data()!["image url"],
+    });
   }
 
   @override
