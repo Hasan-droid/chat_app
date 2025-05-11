31abb81e140f395b5fe3aad51e692021acc7880c
Hasan Baydoun <hasan-net2010@hotmail.com>
2025-05-11 12:32:32 +0300

    300 & 301
    1- just setup notifications and test a message from fir base
    
    ...I'm going to stop the project here left classes are not the important since it demonstrate how to configure the firebase messaging system from firebase not the flutter app itself
    
    ..299 lesson demonstrate how to configure the messaging from firebase on ios

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,10 +1,34 @@
 import 'package:chat_app/widgets/chat_messages.dart';
 import 'package:chat_app/widgets/new_message.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
-class chatScreen extends StatelessWidget {
+class chatScreen extends StatefulWidget {
   chatScreen({super.key});
+
+  @override
+  State<chatScreen> createState() => _chatScreenState();
+}
+
+class _chatScreenState extends State<chatScreen> {
+  //notifications should only received to loggedIn users , which this screen is
+  void setPoshNotifications() async {
+    final fcm = FirebaseMessaging.instance;
+    await fcm.requestPermission();
+
+    //get the address of the user device
+    final token = await fcm.getToken();
+
+    print("virtual token $token");
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    setPoshNotifications();
+  }
 
   @override
   Widget build(BuildContext context) {
